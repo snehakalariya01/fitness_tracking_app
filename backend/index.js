8db0a1d1f2b285809e2841db8908e37185566557
sneha kalariya <snehakalariya01@gmail.com>
2026-06-11 04:25:05 -0500

    added other files

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./db/dbConnection');
+const userRoutes = require('./Routes/UserRoutes');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const workoutRoutes = require('./Routes/workoutRoutes');
+const exerciseRoutes = require('./Routes/exerciseRoutes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/exercises', exerciseRoutes);
+
+app.get('/', (req, res) => {
+  res.send('root is working');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
