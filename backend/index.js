186fc1f1335fbb7f92a26361c08037c2c0d627a1
sneha kalariya <snehakalariya01@gmail.com>
2026-06-11 04:50:35 -0500

    added chllnage route

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const workoutRoutes = require('./Routes/workoutRoutes');
 const exerciseRoutes = require('./Routes/exerciseRoutes');
+const challengeRoutes = require('./Routes/challengeRoutes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/exercises', exerciseRoutes);
+app.use('/api/challenges', challengeRoutes);
 
 app.get('/', (req, res) => {
   res.send('root is working');
